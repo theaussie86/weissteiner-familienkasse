19acf985039d102a2daa454b01b6739eb76c58e2
Christoph Weissteiner <30799919+theaussie86@users.noreply.github.com>
2025-06-20 21:52:02 +0200

    remove crisp

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ import "./globals.css";
 const font = Inter({ subsets: ["latin"] });
 
 export const viewport: Viewport = {
-	// Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
-	themeColor: config.colors.main,
-	width: "device-width",
-	initialScale: 1,
+  // Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
+  themeColor: config.colors.main,
+  width: "device-width",
+  initialScale: 1,
 };
 
 // This adds default SEO tags to all pages in our app.
@@ -20,16 +20,12 @@ export const viewport: Viewport = {
 export const metadata = getSEOTags();
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-	return (
-		<html
-			lang="en"
-			data-theme={config.colors.theme}
-			className={font.className}
-		>
-			<body>
-				{/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
-				<ClientLayout>{children}</ClientLayout>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" data-theme={config.colors.theme} className={font.className}>
+      <body>
+        {/* ClientLayout contains all the client wrappers (toast messages, tooltips, etc.) */}
+        <ClientLayout>{children}</ClientLayout>
+      </body>
+    </html>
+  );
 }
